cea6dcbbe9af8da02d9cf435f44932d8be83990c
Nakamura Kenya <pinpondream@yahoo.co.jp>
2020-03-03 23:48:22 +0900

    add

---
+++ vuejs-projects/my-first-vue-cli-project/src/components/Hellonuxt.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h1>Hello Nuxt!</h1>
+  </div>
+</template>
+
+ <script>
+</script>
+
+
+<style scoped>
+</style>
---
+++ vuejs-projects/my-first-vue-cli-project/src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home">
+    <HelloNuxt></HelloNuxt>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloNuxt from "@/components/Hellonuxt.vue";
+export default {
+  name: "Home",
+  components: {
+    HelloNuxt
+  }
+};
+</script>
